5f3fdde9a97d2b273b5e045e0530409993fee32f
Vinnie8609 <129815485+Vinnie8609@users.noreply.github.com>
2026-02-19 09:33:45 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,43 +1,15 @@
-from torchvision import transforms
 from handlers import THEBE_Handler,FAULTSEG_Handler
 from data import get_THEBE,get_FAULTSEG
 
-from nets_test_transunet import Net, MNIST_Net, SVHN_Net, CIFAR10_Net,THEBE_Net,FAULTSEG_Net
-from query_strategies import RandomSampling, LeastConfidence, MarginSampling, EntropySampling, \
-                             LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
-                             KMeansSampling, KCenterGreedy, BALDDropout, \
-                             AdversarialBIM, AdversarialDeepFool,Orderselect,MaxconfidenceSampling
+from nets_test_transunet import Net,THEBE_Net,FAULTSEG_Net
+from query_strategies import  EntropySampling
+                            
 
-params = {'MNIST':
-              {'n_epoch': 10, 
-               'train_args':{'batch_size': 64, 'num_workers': 1},
-               'test_args':{'batch_size': 1000, 'num_workers': 1},
-               'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-          'FashionMNIST':
-              {'n_epoch': 10, 
-               'train_args':{'batch_size': 64, 'num_workers': 1},
-               'test_args':{'batch_size': 1000, 'num_workers': 1},
-               'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-          'SVHN':
-              {'n_epoch': 20, 
-               'train_args':{'batch_size': 64, 'num_workers': 1},
-               'test_args':{'batch_size': 1000, 'num_workers': 1},
-               'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-          'CIFAR10':
-              {'n_epoch': 20, 
-               'train_args':{'batch_size': 64, 'num_workers': 1},
-               'test_args':{'batch_size': 1000, 'num_workers': 1},
-               'optimizer_args':{'lr': 0.05, 'momentum': 0.3}},
-          'FAULTSEG':
-              {'n_epoch': 100, 
-               'train_args':{'batch_size':16, 'num_workers': 4},
-               'val_args':{'batch_size': 8, 'num_workers': 4},
-               'test_args':{'batch_size': 4, 'num_workers': 4},#50
-               'optimizer_args':{'lr': 0.002, 'momentum': 0.9}},
+params = {
            'THEBE':
-              {'n_epoch': 50, 
+              {'n_epoch': 10, 
                'train_args':{'batch_size':8, 'num_workers': 0},
-               'trainsmall_args':{'batch_size':2, 'num_workers': 0},
+               'trainsmall_args':{'batch_size':16, 'num_workers': 0},
                'val_args':{'batch_size': 8, 'num_workers': 0},
                'test_args':{'batch_size':8 , 'num_workers': 0},#50
                'optimizer_args':{'lr': 0.002, 'momentum': 0.9}}
@@ -59,15 +31,8 @@ def get_dataset(name):
         raise NotImplementedError
         
 def get_net(name, device):
-    if name == 'MNIST':
-        return Net(MNIST_Net, params[name], device)
-    elif name == 'FashionMNIST':
-        return Net(MNIST_Net, params[name], device)
-    elif name == 'SVHN':
-        return Net(SVHN_Net, params[name], device)
-    elif name == 'CIFAR10':
-        return Net(CIFAR10_Net, params[name], device)
-    elif name == 'THEBE':
+
+    if name == 'THEBE':
         return Net(THEBE_Net, params[name], device)
     elif name == 'FAULTSEG':
         return Net(FAULTSEG_Net, params[name], device)
@@ -78,38 +43,11 @@ def get_params(name):
     return params[name]
 
 def get_strategy(name):
-    if name == "RandomSampling":
-        return RandomSampling
-    elif name == "LeastConfidence":
-        return LeastConfidence
-    elif name == "MarginSampling":
-        return MarginSampling
-    elif name == "EntropySampling":
+
+    if name == "EntropySampling":
         return EntropySampling
-    elif name == "LeastConfidenceDropout":
-        return LeastConfidenceDropout
-    elif name == "MarginSamplingDropout":
-        return MarginSamplingDropout
-    elif name == "EntropySamplingDropout":
-        return EntropySamplingDropout
-    elif name == "KMeansSampling":
-        return KMeansSampling
-    elif name == "KCenterGreedy":
-        return KCenterGreedy
-    elif name == "BALDDropout":
-        return BALDDropout
-    elif name == "AdversarialBIM":
-        return AdversarialBIM
-    elif name == "AdversarialDeepFool":
-        return AdversarialDeepFool
-    
-    elif name == "Orderselect":
-        return Orderselect
-    elif name == "MaxconfidenceSampling":
-        return MaxconfidenceSampling
+
+     
     else:
         raise NotImplementedError
     
-# albl_list = [MarginSampling(X_tr, Y_tr, idxs_lb, net, handler, args),
-#              KMeansSampling(X_tr, Y_tr, idxs_lb, net, handler, args)]
-# strategy = ActiveLearningByLearning(X_tr, Y_tr, idxs_lb, net, handler, args, strategy_list=albl_list, delta=0.1)
